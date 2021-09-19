531936ded040f116ab6bbc44f38b03ee6d7c3d7a
Amir Sharapov <amir@amirfounder.com>
2021-09-19 00:15:47 -0500

    added cors configuration

--- src/main/java/com/amirfounder/amirappareldemoapispringboot/config/SecurityConfig.java
+++ src/main/java/com/amirfounder/amirappareldemoapispringboot/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.amirfounder.amirappareldemoapispringboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +19,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .disable()
             .authorizeRequests()
             .antMatchers("*").permitAll();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/*")
+                        .allowedMethods("GET", "PUT", "POST", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
     }
 
 }
