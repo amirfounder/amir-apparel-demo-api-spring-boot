551fe55a48d13b489153b9faf287ab146490e046
Amir Sharapov <amir@amirfounder.com>
2021-09-18 19:09:00 -0500

    Initialized Spring Boot with start.spring.io

---
+++ src/main/java/com/amirfounder/amirappareldemoapispringboot/AmirApparelDemoApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.amirfounder.amirappareldemoapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmirApparelDemoApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmirApparelDemoApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amirfounder/amirappareldemoapispringboot/AmirApparelDemoApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amirfounder.amirappareldemoapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmirApparelDemoApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
