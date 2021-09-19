047eae3db361fff1fc420e7fe5b914d94cb6fe98
Amir Sharapov <amir@amirfounder.com>
2021-09-18 19:56:14 -0500

    create the data loader and product factory

--- src/main/java/com/amirfounder/amirappareldemoapispringboot/AppRunner.java
+++ src/main/java/com/amirfounder/amirappareldemoapispringboot/AppRunner.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmirApparelDemoApiSpringBootApplication {
+public class AppRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AmirApparelDemoApiSpringBootApplication.class, args);
+		SpringApplication.run(AppRunner.class, args);
 	}
 
 }
--- src/main/java/com/amirfounder/amirappareldemoapispringboot/domains/product/ProductRepository.java
+++ src/main/java/com/amirfounder/amirappareldemoapispringboot/domains/product/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.amirfounder.amirappareldemoapispringboot.domains.product;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
